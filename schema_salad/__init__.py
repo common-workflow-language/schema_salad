a81a35f55040a73478d91792d0c42b566368bbfc
Manvendra Singh <manvendra0310@gmail.com>
2017-08-01 00:59:31 +0530

    schema_salad/__init__.py: Create '\tmp' folder on windows if not present

--- schema_salad/__init__.py
+++ schema_salad/__init__.py
@@ -1,8 +1,9 @@
 from __future__ import absolute_import
 import logging
+import os
 import sys
 import typing
-
+from .utils import onWindows
 __author__ = 'peter.amstutz@curoverse.com'
 
 _logger = logging.getLogger("salad")
@@ -11,5 +12,15 @@ _logger.setLevel(logging.INFO)
 
 import six
 if six.PY3:
+
+    if onWindows:
+        # create '/tmp' folder if not present
+        # required by autotranslate module
+        if not os.path.exists("/tmp"):
+            try:
+                os.mkdirs("/tmp")
+            except OSError as exception:
+                exit(0)
+
     from past import autotranslate  # type: ignore
     autotranslate(['avro', 'avro.schema'])
