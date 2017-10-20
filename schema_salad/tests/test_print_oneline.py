d1431e6980742e2104036bf48f00002f7844f8ec
Tomoya Tanjo <ttanjo@gmail.com>
2017-10-20 18:09:10 +0900

    Fix for path conversion

--- schema_salad/tests/test_print_oneline.py
+++ schema_salad/tests/test_print_oneline.py
@@ -41,8 +41,8 @@ class TestPrintOneline(unittest.TestCase):
             except RuntimeError as e:
                 msg = re.sub(r'[\s\n]+', ' ', strip_dup_lineno(six.text_type(e)))
                 # convert Windows path to Posix path
-                if re.match(r'\\'):
-                    fullpath = re.sub(r'\\', '/')
+                if '\\' in fullpath:
+                    fullpath = re.replace('\\', '/')
                     fullpath = '/'+fullpath
                 self.assertEqual(msg, 'while scanning a simple key in "file://%s", line 9, column 7 could not find expected \':\' in "file://%s", line 10, column 1' % (fullpath, fullpath))
                 print("\n", e)
