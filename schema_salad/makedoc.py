eee998a6ebdca253365e3a4d8024498c859e310c
Michael R. Crusoe <1330696+mr-c@users.noreply.github.com>
2018-06-30 19:59:57 -0700

    add missing import (#179)
    * add missing import
    
    * clarify type

--- schema_salad/makedoc.py
+++ schema_salad/makedoc.py
@@ -10,6 +10,8 @@ import re
 import sys
 import logging
 from io import open
+import codecs
+from codecs import StreamWriter  # pylint: disable=unused-import
 
 from . import schema
 from .utils import add_dictlist, aslist
@@ -422,8 +424,14 @@ class RenderType(object):
             self.render_type(self.typemap[s], depth)
 
 
-def avrold_doc(j, outdoc, renderlist, redirects, brand, brandlink, primtype):
-    # type: (List[Dict[Text, Any]], IO[Any], str, Dict, str, str, str) -> None
+def avrold_doc(j,           # type: List[Dict[Text, Any]]
+               outdoc,      # type: Union[IO[Any], StreamWriter]
+               renderlist,  # type: str
+               redirects,   # type: Dict
+               brand,       # type: str
+               brandlink,   # type: str
+               primtype     # type: str
+              ):  # type: (...) -> None
     toc = ToC()
     toc.start_numbering = False
 
