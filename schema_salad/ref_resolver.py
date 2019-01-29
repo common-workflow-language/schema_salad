6c47d16e11c6fbeb5c5edf207a8308d242552d95
Peter Amstutz <pamstutz@veritasgenetics.com>
2019-01-29 13:01:13 -0500

    Fix "metadata" (toplevel file attributes) from resolve_ref

--- schema_salad/ref_resolver.py
+++ schema_salad/ref_resolver.py
@@ -573,7 +573,13 @@ class Loader(object):
         url = self.expand_url(lref, base_url, scoped_id=(obj is not None))
         # Has this reference been loaded already?
         if url in self.idx and (not mixin):
-            return self.idx[url], {}
+            resolved_obj = self.idx[url]
+            metadata = self.idx[urllib.parse.urldefrag(url)[0]]
+            if u"$graph" in resolved_obj:
+                metadata = _copy_dict_without_key(resolved_obj, u"$graph")
+                return resolved_obj[u"$graph"], metadata
+            else:
+                return resolved_obj, metadata
 
         sl.raise_type = RuntimeError
         with sl:
@@ -917,18 +923,16 @@ class Loader(object):
             if newctx is not None:
                 loader = newctx
 
+            for identifer in loader.identity_links:
+                if identifer in document:
+                    if isinstance(document[identifer], string_types):
+                        document[identifer] = loader.expand_url(
+                            document[identifer], base_url, scoped_id=True)
+                        loader.idx[document[identifer]] = document
+
+            metadata = document
             if u"$graph" in document:
-                metadata = _copy_dict_without_key(document, u"$graph")
                 document = document[u"$graph"]
-                resolved_metadata = loader.resolve_all(
-                    metadata, base_url, file_base=file_base,
-                    checklinks=False)[0]
-                if isinstance(resolved_metadata, MutableMapping):
-                    metadata = resolved_metadata
-                else:
-                    raise validate.ValidationException(
-                        "Validation error, metadata must be dict: %s"
-                        % (resolved_metadata))
 
         if isinstance(document, CommentedMap):
             self._normalize_fields(document, loader)
@@ -984,13 +988,6 @@ class Loader(object):
                 _logger.warning("failed", exc_info=True)
                 raise validate.ValidationException("(%s) (%s) Validation error in position %i:\n%s" % (
                     id(loader), file_base, i, validate.indent(Text(v))))
-
-            for identifer in loader.identity_links:
-                if identifer in metadata:
-                    if isinstance(metadata[identifer], string_types):
-                        metadata[identifer] = loader.expand_url(
-                            metadata[identifer], base_url, scoped_id=True)
-                        loader.idx[metadata[identifer]] = document
 
         if checklinks:
             all_doc_ids={}  # type: Dict[Text, Text]
