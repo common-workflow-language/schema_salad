5b9341197d53e53fa211feefcca847e9ae115222
Tomoya Tanjo <ttanjo@gmail.com>
2019-02-08 14:14:53 +0900

    Add test for test3.cwl

--- schema_salad/tests/test_errors.py
+++ schema_salad/tests/test_errors.py
@@ -66,6 +66,22 @@ class TestErrors(unittest.TestCase):
                                  str(e.exception)),
                         str(e.exception) + ' is not matched.')
 
+    def test_error_message3(self):
+        document_loader, avsc_names, schema_metadata, metaschema_loader = load_schema(
+            get_data(u"tests/test_schema/CommonWorkflowLanguage.yml"))
+
+        t = "test_schema/test3.cwl"
+        with self.assertRaises(ValidationException) as e:
+            load_and_validate(document_loader, avsc_names,
+                              six.text_type(get_data("tests/"+t)), True)
+        self.assertTrue(re.match(r'''
+^.+test3\.cwl:5:1: checking field `outputs`
+^.+test3\.cwl:6:3:   checking object `test_schema/test3.cwl#bar`
+\s+Field `type` references unknown identifier `xstring`, tried
+\s+file://.+/schema_salad/tests/test_schema/test3.cwl#xstring$'''[1:],
+                                 str(e.exception)),
+                        str(e.exception) + ' is not matched.')
+
     @unittest.skip("See https://github.com/common-workflow-language/common-workflow-language/issues/734")
     def test_errors_previously_defined_dict_key(self):
         document_loader, avsc_names, schema_metadata, metaschema_loader = load_schema(
