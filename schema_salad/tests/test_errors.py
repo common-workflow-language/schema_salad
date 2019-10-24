081e5268fa74fac80dd0f00b5538e72d78625ebf
Tomoya Tanjo <ttanjo@gmail.com>
2019-10-24 13:03:51 +0900

    Fix test

--- schema_salad/tests/test_errors.py
+++ schema_salad/tests/test_errors.py
@@ -126,7 +126,7 @@ def test_error_message5():
 \s+tried `Workflow`\s+but
 .+test5\.cwl:7:1:     the `steps`\s+field\s+is\s+not valid\s+because
 \s+tried array\s+of\s+<WorkflowStep>\s+but
-.+test5\.cwl:7:9:         item is\s+invalid because
+.+test5\.cwl:7:9:         item is\s+invalid\s+because
 \s+is not a\s+dict$"""[
         1:
     ]
@@ -147,10 +147,10 @@ def test_error_message7():
 \s+tried `Workflow`\s+but
 .+test7\.cwl:7:1:     the `steps`\s+field\s+is\s+not valid\s+because
 \s+tried array\s+of\s+<WorkflowStep>\s+but
-.+test7\.cwl:8:3:         item is\s+invalid because
-\s+\* missing\s+required\s+field `run`
+.+test7\.cwl:8:3:         item is\s+invalid\s+because
+\s+\* missing\s+required\s+field\s+`run`
 .+test7\.cwl:9:5:           \* invalid\s+field\s+`scatter_method`,\s+expected\s+one """
-        + r"""of:\s+'id', 'in', 'out',\s+'requirements',\s+'hints',\s+"""
+        + r"""of:\s+'id',\s+'in', 'out',\s+'requirements',\s+'hints',\s+"""
         + r"""'label',\s+'doc',\s+'run',\s+'scatter',\s+'scatterMethod'$"""
     )
     with pytest.raises(ValidationException, match=match):
@@ -207,8 +207,8 @@ def test_error_message10():
 \s+tried `Workflow`\s+but
 .+test10\.cwl:7:1:     the `steps`\s+field is\s+not valid\s+because
 \s+tried array\s+of\s+<WorkflowStep>\s+but
-.+test10\.cwl:8:3:         item is\s+invalid because
-\s+\* missing\s+required\s+field `run`
+.+test10\.cwl:8:3:         item is\s+invalid\s+because
+\s+\* missing\s+required\s+field\s+`run`
 .+test10\.cwl:9:5:           \* the\s+`scatterMethod`\s+field is\s+not valid\s+because
 \s+value\s+is a\s+CommentedSeq,\s+expected\s+null\s+or\s+ScatterMethod$"""[
         1:
@@ -249,12 +249,12 @@ def test_error_message15():
 \s+tried\s+`CommandLineTool`\s+but
 .+test15\.cwl:6:1:\s+the `inputs`\s+field\s+is not valid\s+because
 .+test15\.cwl:7:3:\s+item is\s+invalid\s+because
-.+test15\.cwl:9:5:\s+the\s+`inputBinding`\s+field is not\s+valid\s+because
+.+test15\.cwl:9:5:\s+the\s+`inputBinding`\s+field is\s+not\s+valid\s+because
 .+tried\s+CommandLineBinding\s+but
-.+test15\.cwl:11:7:             \*\s+invalid field\s+`invalid_field`,\s+expected """
+.+test15\.cwl:11:7:             \*\s+invalid\s+field\s+`invalid_field`,\s+expected """
         + r"""one\s+of:\s+'loadContents',\s+'position',\s+'prefix',\s+'separate',"""
         + r"""\s+'itemSeparator',\s+'valueFrom',\s+'shellQuote'
-.+test15\.cwl:12:7:             \*\s+invalid field\s+`another_invalid_field`,"""
+.+test15\.cwl:12:7:             \*\s+invalid\s+field\s+`another_invalid_field`,"""
         + r"""\s+expected one\s+of:\s+'loadContents',\s+'position',\s+'prefix',"""
         + r"""\s+'separate',\s+'itemSeparator',\s+'valueFrom',\s+'shellQuote'$"""
     )
