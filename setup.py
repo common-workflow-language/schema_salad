d563b93939446ec0f29088e393db5fc6458b6c88
Michael R. Crusoe <michael.crusoe@gmail.com>
2020-04-16 13:09:09 +0200

    allow newer rdflib-jsonld

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ install_requires = [
     # once the minimum version for ruamel.yaml >= 0.15.99
     # then please update the mypy targets in the Makefile
     'rdflib >= 4.2.2, < 4.3.0',
-    'rdflib-jsonld >= 0.3.0, < 0.5.0',
+    'rdflib-jsonld >= 0.3.0, < 0.6.0',
     'mistune >= 0.8.1, < 0.9',
     'CacheControl >= 0.11.7, < 0.12',
     'lockfile >= 0.9',
