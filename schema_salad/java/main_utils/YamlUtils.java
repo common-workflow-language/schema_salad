e3b642bd7274f74935ef0b0799d67074e7c59179
Michael R. Crusoe <michael.crusoe@gmail.com>
2021-01-21 18:15:06 +0100

    java: load yaml safely

--- schema_salad/java/main_utils/YamlUtils.java
+++ schema_salad/java/main_utils/YamlUtils.java
@@ -3,17 +3,18 @@ package ${package}.utils;
 import java.util.List;
 import java.util.Map;
 import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.constructor.SafeConstructor;
 
 public class YamlUtils {
 
   public static Map<String, Object> mapFromString(final String text) {
-    Yaml yaml = new Yaml();
+    Yaml yaml = new Yaml(new SafeConstructor());
     final Map<String, Object> result = yaml.load(text);
     return result;
   }
 
   public static List<Object> listFromString(final String text) {
-    Yaml yaml = new Yaml();
+    Yaml yaml = new Yaml(new SafeConstructor());
     final List<Object> result = yaml.load(text);
     return result;
   }
