a33519beeace92927d4cd269411f113c2d2b1980
Michael R. Crusoe <michael.crusoe@gmail.com>
2021-06-24 11:19:51 +0200

    cleanups

--- schema_salad/tests/test_avro_names.py
+++ schema_salad/tests/test_avro_names.py
@@ -1,11 +1,9 @@
-from schema_salad.avro.schema import Names, SchemaParseException
-from schema_salad.ref_resolver import Loader
-from schema_salad.schema import load_and_validate, load_schema
+from schema_salad.schema import load_schema
 
 from .util import get_data
 
-def test_avro_loading():
+
+def test_avro_loading() -> None:
     path = get_data("tests/test_schema/avro_naming.yml")
     assert path
     document_loader, avsc_names, schema_metadata, metaschema_loader = load_schema(path)
-    
