db52eef6269808953b0b5a3a17faec1989bc9add
Peter Amstutz <peter.amstutz@curii.com>
2021-11-16 16:15:13 -0500

    Fix mypy

--- schema_salad/tests/test_avro_names.py
+++ schema_salad/tests/test_avro_names.py
@@ -1,5 +1,6 @@
 """Avro related tests."""
 from schema_salad.schema import load_schema
+from schema_salad.avro.schema import Names
 
 from .util import get_data
 
@@ -9,4 +10,5 @@ def test_avro_loading() -> None:
     path = get_data("tests/test_schema/avro_naming.yml")
     assert path
     document_loader, avsc_names, schema_metadata, metaschema_loader = load_schema(path)
+    assert isinstance(avsc_names, Names)
     assert avsc_names.get_name("com.example.derived_schema.ExtendedThing", None)
