8322764332086dc09709bc377da9405db9d58440
Michael R. Crusoe <michael.crusoe@gmail.com>
2022-07-16 20:54:25 -0500

    bump minimum setuptools version

--- release-test.sh
+++ release-test.sh
@@ -19,7 +19,7 @@ else
 fi
 run_tests="bin/py.test --pyargs ${module}"
 pipver=20.3.3  # minimum required version of pip for Python 3.10
-setuptoolsver=50.0.0  # required for Python 3.10
+setuptoolsver=50.0.1  # required for Python 3.10
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 rm -Rf testenv? || /bin/true
