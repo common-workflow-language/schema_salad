470589de5d2bacc3921cb24c40aec00e710be025
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2022-07-21 21:26:58 +1200

    Add a auto-loaded global fixture to clear the metaschema cache

---
+++ conftest.py
@@ -0,0 +1,12 @@
+import pytest
+import schema_salad.schema
+
+@pytest.fixture(autouse=True, scope='function')
+def isolated_cache():
+    """A fixture to clear the schema_salad metaschema cache.
+
+    Auto-loaded (see autouse) fixture, loaded per test (function scope).
+    Prevents issues when running multiple tests that load metaschemas
+    multiple times or in parallel (`pytest-parallel`, `pytest-xdist`, etc).
+    """
+    schema_salad.schema.cached_metaschema = None
