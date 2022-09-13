120e991a78a23dc16dd92c7a3d40900947c7d3b4
Michael R. Crusoe <1330696+mr-c@users.noreply.github.com>
2022-09-13 12:57:18 +0200

    update CacheControl stubs via https://github.com/ionrock/cachecontrol/pull/279/ (#599)

--- mypy-stubs/cachecontrol/controller.pyi
+++ mypy-stubs/cachecontrol/controller.pyi
@@ -1,17 +1,18 @@
+from typing import Collection
+
 from _typeshed import Incomplete
 
-logger: Incomplete
-URI: Incomplete
+from .cache import BaseCache
 
 class CacheController:
-    cache: Incomplete
-    cache_etags: Incomplete
+    cache: BaseCache
+    cache_etags: bool
     serializer: Incomplete
-    cacheable_status_codes: Incomplete
+    cacheable_status_codes: Collection[int] | None = None
     def __init__(
         self,
-        cache: Incomplete | None = ...,
-        cache_etags: bool = ...,
-        serializer: Incomplete | None = ...,
-        status_codes: Incomplete | None = ...,
+        cache: BaseCache | None = None,
+        cache_etags: bool = True,
+        serializer: Incomplete | None = None,
+        status_codes: Collection[int] | None = None,
     ) -> None: ...
--- mypy-stubs/cachecontrol/wrapper.pyi
+++ mypy-stubs/cachecontrol/wrapper.pyi
@@ -1,13 +1,18 @@
+from typing import Collection, Type
+
 from _typeshed import Incomplete
 from requests import Session
 
+from .cache import BaseCache
+from .controller import CacheController
+
 def CacheControl(
     sess: Session,
-    cache: Incomplete | None = ...,
-    cache_etags: bool = ...,
-    serializer: Incomplete | None = ...,
-    heuristic: Incomplete | None = ...,
-    controller_class: Incomplete | None = ...,
-    adapter_class: Incomplete | None = ...,
-    cacheable_methods: Incomplete | None = ...,
+    cache: BaseCache | None = None,
+    cache_etags: bool = True,
+    serializer: Incomplete | None = None,
+    heuristic: Incomplete | None = None,
+    controller_class: Type[CacheController] | None = None,
+    adapter_class: Incomplete | None = None,
+    cacheable_methods: Collection[str] | None = None,
 ) -> Session: ...
