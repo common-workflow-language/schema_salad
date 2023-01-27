13395631183f7788c7a3a143051d8b3f298b09eb
Michael R. Crusoe <michael.crusoe@gmail.com>
2023-01-27 15:00:55 +0100

    script to trigger a manual wheel publish event for a particular release

---
+++ manual_wheel_publish.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# call with your GitHub personal access token, with at least "repo" permissions
+# and the target version tag
+curl \
+  -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $1"\
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/common-workflow-language/schema_salad/dispatches \
+  -d "{\"event_type\":\"on-demand-wheel\",\"client_payload\":{\"ref\":\"$2\"}}"
