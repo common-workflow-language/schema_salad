36e30805b47e9d0b66d9e11f400d43aa14258308
Michael R. Crusoe <michael.crusoe@gmail.com>
2023-02-01 14:25:54 +0100

    test container building

--- build-schema_salad-docker.sh
+++ build-schema_salad-docker.sh
@@ -2,9 +2,9 @@
 set -ex
 docker build --file=schema_salad.Dockerfile --tag=quay.io/commonwl/schema_salad .
 docker run quay.io/commonwl/schema_salad /bin/sh -c \
-	'pip install pytest-xdist && pytest --pyargs schema_salad -n auto'
+	'apk add --no-cache py3-pip && pip install pytest-xdist && pytest --pyargs schema_salad -n auto'
 
-version=$(git describe --tags)
-if echo "$version" | grep -vq '\-' >& /dev/null ; then
-    docker tag quay.io/commonwl/schema_salad quay.io/commonwl/schema_salad:"$version"
-fi
+# version=$(git describe --tags)
+# if echo "$version" | grep -vq '\-' >& /dev/null ; then
+#     docker tag quay.io/commonwl/schema_salad quay.io/commonwl/schema_salad:"$version"
+# fi
