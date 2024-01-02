ae3f353ff614dce9c021c815e8345c7082c7272d
Michael R. Crusoe <michael.crusoe@gmail.com>
2024-01-02 16:51:05 +0100

    fix container testing

--- build-schema_salad-docker.sh
+++ build-schema_salad-docker.sh
@@ -3,7 +3,7 @@ set -ex
 engine=${ENGINE:-docker}  # example: `ENGINE=podman ./build-schema_salad-docker.sh`
 ${engine} build --file=schema_salad.Dockerfile --tag=quay.io/commonwl/schema_salad .
 ${engine} run quay.io/commonwl/schema_salad /bin/sh -c \
-	'apk add --no-cache py3-pip && pip install pytest-xdist && cd /tmp && pytest --pyargs schema_salad -n auto'
+	'apk add --no-cache py3-pip && pip install --break-system-packages pytest-xdist && cd /tmp && pytest --pyargs schema_salad -n auto'
 
 # version=$(git describe --tags)
 # if echo "$version" | grep -vq '\-' >& /dev/null ; then
