80bd58758f042ae90a96dcb312abbd3efb49dddf
Enrico Seiler <enrico.seiler@hotmail.de>
2024-07-26 16:18:31 +0200

    Fix C++23: ~heap_object() with incomplete types

--- schema_salad/tests/cpp_tests/02_two_records.h
+++ schema_salad/tests/cpp_tests/02_two_records.h
@@ -200,7 +200,7 @@ public:
         *data = std::forward<T2>(oth);
     }
 
-    ~heap_object() = default;
+    ~heap_object();
 
     auto operator=(heap_object const& oth) -> heap_object& {
         *data = *oth;
@@ -255,6 +255,9 @@ struct MyRecordTwo {
     virtual void fromYaml(YAML::Node const& n);
 };
 }
+
+template <typename T>
+heap_object<T>::~heap_object() = default;
 
 inline auto https___example_com_::MyRecordOne::toYaml() const -> YAML::Node {
     using ::toYaml;
