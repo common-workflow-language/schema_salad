519a11e5a7ef523a5182c6670b2eedd78d378a81
GlassOfWhiskey <iacopo.c92@gmail.com>
2025-12-19 23:37:07 +0100

    Make abstract types abstract

--- schema_salad/python_codegen.py
+++ schema_salad/python_codegen.py
@@ -154,6 +154,7 @@ class PythonCodeGen(CodeGenBase):
             ext = "Saveable"
 
         if self.current_class_is_abstract:
+            ext += ", metaclass=ABCMeta"
             decorator = "@trait\n"
         else:
             decorator = ""
